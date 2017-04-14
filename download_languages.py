7a0fbc31b4336fde00d2fe45b478c57dd12560c8
shteken <barshteken@gmail.com>
2017-04-14 12:56:41 +0300

    Update download_languages.py
    update exceptions handling

--- download_languages.py
+++ download_languages.py
@@ -1,11 +1,12 @@
-# usage: python github/download_relevant_data.py <day> - parse all issue of given day
-
+# This code updates the projects json with languages until it reaches the maximum requests limit.
+# We have only 6000 requests per hour (GitHub limits)
 import gzip
 import json
 import sys
 from github import Github
 
 
+# finishes our session for now and updates the file
 def finalize():
     print('finished for now... writing data and exiting.')
     with open('processed_data/2015-01.json', 'w') as outfile:
@@ -16,18 +17,19 @@ with open('github/config', 'r') as config_file:
     user, password = config_file.readline().split(' ')
 g = Github(user, password)  # insert github user
 
+# read the projects file
 with open('processed_data/2015-01.json', 'r') as data_file:
     projects = json.load(data_file)
 for repo in projects.keys():
     try:
         print("getting language for {}".format(repo))
-        if 'languages' not in projects[repo]:
+        if 'languages' not in projects[repo]:  # get the languages for a project
             projects[repo]['languages'] = g.get_repo(int(repo)).get_languages()
     except Exception as e:
-        if str(e)[0:3] == '403':
-            finalize()
-        else:
-            projects[repo]['languages'] = {}
         print(e)
+        if 'Maximum number of login attempts exceeded. Please try again later.' in str(e) or 'API rate limit exceeded' in str(e):
+            finalize()  # we are out of requests
+        else:
+            projects[repo]['languages'] = {}  # no languages for this project
 
 finalize()
